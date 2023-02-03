3ad9327e4323c62cf61bcb8781433404b6a07010
PaAdRaAg <pramos12@ucol.mx>
2023-02-03 17:34:24 -0600

    min

--- privada/index.php
+++ privada/index.php
@@ -14,42 +14,42 @@
 
 
 
-////////////GUENA
-// include '../db_conn.php';
+  ////////////GUENA
+  // include '../db_conn.php';
 
-// $atributos = $saml->getAttributes(); //Obtiene sus atributos
+  // $atributos = $saml->getAttributes(); //Obtiene sus atributos
 
-// $nocuenta = $atributos["uCuenta"][0];
-// $nombre = $atributos["sn"][0];
-// $apellido = $atributos["givenName"][0];
-// $email = $atributos["uCorreo"][0];
+  // $nocuenta = $atributos["uCuenta"][0];
+  // $nombre = $atributos["sn"][0];
+  // $apellido = $atributos["givenName"][0];
+  // $email = $atributos["uCorreo"][0];
 
-// $sql = "INSERT INTO crud (id, nocuenta, nombre, apellido, email)
-//     VALUES (NULL, $nocuenta, '$nombre', '$apellido', '$email')";
+  // $sql = "INSERT INTO crud (id, nocuenta, nombre, apellido, email)
+  //     VALUES (NULL, $nocuenta, '$nombre', '$apellido', '$email')";
 
-// $result = mysqli_query($conn, $sql);
-///////////////////////gurna2
-// include '../db_conn.php';
+  // $result = mysqli_query($conn, $sql);
+  ///////////////////////gurna2
+  // include '../db_conn.php';
 
 
-// $atributos = $saml->getAttributes(); //Obtiene sus atributos
+  // $atributos = $saml->getAttributes(); //Obtiene sus atributos
 
-// $nocuenta = $atributos["uCuenta"][0];
-// $nombre = $atributos["sn"][0];
-// $apellido = $atributos["givenName"][0];
-// $email = $atributos["uCorreo"][0];
+  // $nocuenta = $atributos["uCuenta"][0];
+  // $nombre = $atributos["sn"][0];
+  // $apellido = $atributos["givenName"][0];
+  // $email = $atributos["uCorreo"][0];
 
-// $sql = "SELECT * FROM crud WHERE email = '$email'";
-// $result = mysqli_query($conn, $sql);
-// if (mysqli_num_rows($result) > 0) {
-//   echo "Location: ../index.php"
-// } else {
-//   $sql = "INSERT INTO crud (id, nombre, apellido, email)
-//   VALUES (NULL, '$nombre', '$apellido', '$email')";
-//   if (mysqli_query($conn, $sql)) {
-//       echo "Value inserted successfully";
-//   } else {
-//       echo "Error inserting value: " . mysqli_error($conn);
-//   }
-// }
+  // $sql = "SELECT * FROM crud WHERE email = '$email'";
+  // $result = mysqli_query($conn, $sql);
+  // if (mysqli_num_rows($result) > 0) {
+  //   echo "Location: ../index.php"
+  // } else {
+  //   $sql = "INSERT INTO crud (id, nombre, apellido, email)
+  //   VALUES (NULL, '$nombre', '$apellido', '$email')";
+  //   if (mysqli_query($conn, $sql)) {
+  //       echo "Value inserted successfully";
+  //   } else {
+  //       echo "Error inserting value: " . mysqli_error($conn);
+  //   }
+  // }
 ?>
